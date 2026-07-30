3c61d4f3458ce575c2259a23845209ebf996a983
Arley Hoyos <arleyhoyosalzate@gmail.com>
2026-07-30 17:44:31 -0500

    chore(backend): initialize Spring Boot and MySQL foundation

---
+++ backend/src/test/java/com/arleyhoyos/incidentmanagement/IncidentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arleyhoyos.incidentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
